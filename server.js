236e2f913ee40ef921e1e5a7d7940eb20c3f8892
Paige Burlew <paigeburlew@gmail.com>
2020-11-09 19:06:03 -0600

    restructured files and added models, public, routes folders

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var PORT = process.env.PORT || 8080;
 var app = express();
 
 
-// Serve static content for the app from the "public" directory in the application directory.
+// Serve static content for the app from the "public" directory in the application directory
 app.use(express.static("public"));
 
-// Parse application body
+// Parse application bodyß
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
