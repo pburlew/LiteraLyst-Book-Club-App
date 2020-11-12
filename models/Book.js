b8e7d7a3730b110a05cdc34845cad2919a587aad
Nic Schworer <nic.schworer@gmail.com>
2020-11-11 21:19:44 -0500

    added sql models and seed data

---
+++ models/Book.js
@@ -0,0 +1,38 @@
+module.exports = function(sequelize, DataTypes) {
+   const Book = sequelize.define("Book", {
+      title: {
+         type: DataTypes.STRING,
+         allowNull: false,
+         validate: {
+            len: [1]
+         }
+      },
+      author: {
+         type: DataTypes.STRING,
+         allowNull: false,
+         validate: {
+            len: [1]
+         }
+      }, 
+      synopsis: {
+         type: DataTypes.TEXT,
+         allowNull: false,
+         len: [1]
+      }, 
+      cover: {
+         type: DataTypes.TEXT,
+         allowNull: false,
+         len: [1]
+      }, 
+      status: {
+         type: DataTypes.STRING,
+         allowNull: false,
+         validate: {
+            len: [1]
+         }
+      }
+   }, {
+      carset: "utf8",
+   });
+   return Book;
+};
---
+++ models/Review.js
@@ -0,0 +1,24 @@
+module.exports = function(sequelize, DataTypes) {
+   const Review = sequelize.define("Review", {
+      bookId: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+      },
+
+      authorId: {
+         type: DataTypes.INTEGER,
+         allowNull: false,
+      }, 
+
+      body: {
+         type: DataTypes.TEXT,
+         allowNull: false,
+         len: [1]
+      }, 
+   },{
+      carset: "utf8",
+   });
+
+    
+   return Review;
+};
